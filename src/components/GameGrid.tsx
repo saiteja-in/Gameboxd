b66843ec7599e8731a0c61a7805c54a485337a20
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-25 13:13:30 +0530

    displaying platfom icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,7 +10,7 @@ const GameGrid = () => {
     const{games,error}=useGames()
     return (
         <>
-       {error && <Text>{error}</Text>};
+       {error && <Text>{error}</Text>}
         <SimpleGrid columns={{sm:1,md:2,lg:3,xl:4}} padding={'10px'} spacing={10}>
             {games.map((game)=>(
                 <GameCard key={game.id} game={game}/>
