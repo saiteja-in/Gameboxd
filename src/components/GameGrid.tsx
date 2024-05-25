3c808ae84f3855389f35bd6d380bbe452c109107
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-25 13:46:02 +0530

    added game card skeletons for good user experience

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,15 +3,17 @@ import  { useEffect, useState } from 'react'
 import useGames from '../Hooks/useGames';
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import GameCard from './GameCard';
+import GameCardSkeleton from './GameCardSkeleton';
 
 
 const GameGrid = () => {
-    
-    const{games,error}=useGames()
+    const skeletons=[1,2,3,4,5,6]
+    const{games,error,isLoading}=useGames()
     return (
         <>
        {error && <Text>{error}</Text>}
         <SimpleGrid columns={{sm:1,md:2,lg:3,xl:4}} padding={'10px'} spacing={10}>
+            {isLoading && skeletons.map((skeleton)=><GameCardSkeleton key={skeleton} />)}
             {games.map((game)=>(
                 <GameCard key={game.id} game={game}/>
             ))}
