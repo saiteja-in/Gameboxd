591383737f6eb60d3a0ecfbc047cb7bbf8972724
saiteja-in <vurukondasaiteja13@gmail.com>
2024-05-25 10:34:05 +0530

    build a resopnsive laylout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
+import React from "react";
+import { Button, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <Button colorScheme='blue'>Button</Button>
-    </div>
-  )
-}
+    <Grid templateAreas={{
+      base:`"nav " " main"`,
+      lg:`"nav nav" "side main"`
+    }}>
+      <GridItem area={"nav"} bg="red">
+        Nav
+      </GridItem>
+      <Show above='lg'>
+      <GridItem area={"side"} bg="yellow">
+        side
+      </GridItem>
+      </Show>
+      <GridItem area={"main"} bg="green">
+        main
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
